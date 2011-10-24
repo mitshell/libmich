546d78bffab1f2c3fd04c08b2b997441507a174f
mitshell <michau.benoit@gmail.com>
2011-10-24 22:59:56 +0200

    add license header

--- libmich/formats/pcap.py
+++ libmich/formats/pcap.py
@@ -1,3 +1,31 @@
+# −*− coding: UTF−8 −*−
+#/**
+# * Software Name : libmich 
+# * Version : 0.2.1 
+# *
+# * Copyright © 2011. Benoit Michau. France Telecom.
+# *
+# * This program is free software: you can redistribute it and/or modify
+# * it under the terms of the GNU General Public License version 2 as published
+# * by the Free Software Foundation. 
+# *
+# * This program is distributed in the hope that it will be useful,
+# * but WITHOUT ANY WARRANTY; without even the implied warranty of
+# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# * GNU General Public License for more details. 
+# *
+# * You will find a copy of the terms and conditions of the GNU General Public
+# * License version 2 in the "license.txt" file or
+# * see http://www.gnu.org/licenses/ or write to the Free Software Foundation,
+# * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+# *
+# *--------------------------------------------------------
+# * File Name : formats/MPEG2.py
+# * Created : 2011-10-24 
+# * Authors : Benoit Michau 
+# *--------------------------------------------------------
+#*/ 
+
 #!/usr/bin/env python
 
 # generic imports
