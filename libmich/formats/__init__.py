7e80e3bb4fdcfdb71066a906f255488387415094
mitshell <michau.benoit@gmail.com>
2011-10-24 22:59:25 +0200

    little correction / update

--- libmich/formats/__init__.py
+++ libmich/formats/__init__.py
@@ -1,4 +1,4 @@
 #
-__all__ = ['pcap', 'IP', 'SCTP', 'SIGTRAN', 'IKEv2', 'EAP', 'EAPAKA', 'uTunnel', \
+__all__ = ['pcap', 'IP', 'SCTP', 'SIGTRAN', 'BGP4', 'IKEv2', 'EAP', 'EAPAKA', \
            'UMA', 'L3Mobile', 'L3Mobile_24007', 'L3Mobile_MM', 'L3Mobile_CC', 'L3Mobile_IE', \
            'MPEG4', 'MPEG2', 'PNG', "RTP"]
