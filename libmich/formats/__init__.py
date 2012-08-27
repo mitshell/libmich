3f4e461c8b4015bf5ba8afbb142c3da96b09f992
mitshell <michau.benoit@gmail.com>
2012-08-27 22:03:31 +0200

    add GPRS mobility management messages -TS 24.008, section 9.4-

--- libmich/formats/__init__.py
+++ libmich/formats/__init__.py
@@ -1,5 +1,6 @@
 #
-__all__ = ['pcap', 'IP', 'SCTP', 'SIGTRAN', 'BGP4', 'IKEv2', 'EAP', 'EAPAKA', 'GTP', \
-           'UMA', 'L3Mobile', 'L3Mobile_24007', 'L3Mobile_MM', 'L3Mobile_CC', 'L3Mobile_IE', \
-           'L1CTL', 'L2GSM', 'L3GSM_RR', 'L3GSM_IE', 'L3GSM_rest', 'MCCMNC', 'IEEE802154', \
+__all__ = ['pcap', 'IP', 'SCTP', 'SIGTRAN', 'BGP4', 'IKEv2', 'EAP', 'EAPAKA', \
+           'UMA', 'L3Mobile', 'L3Mobile_24007', 'L3Mobile_MM', 'L3Mobile_CC', \
+           'L3Mobile_PS_MM', 'L3Mobile_IE', \
+           'L1CTL', 'L2GSM', 'L3GSM_RR', 'L3GSM_IE', 'L3GSM_rest', \
            'MPEG4', 'MPEG2', 'PNG', 'BMP', 'JPEG', 'RTP']
