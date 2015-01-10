8375484b6ac466c1cd6239d0c223e2f160d26151
mitshell <michau.benoit@gmail.com>
2015-01-10 15:17:15 +0100

    update version and packages

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from distutils.core import setup
 setup(name="libmich",
       author="Benoit Michau",
       author_email="michau.benoit@gmail.com",
-      url="http://michau.benoit.free.fr/codes/libmich/",
+      url="http://michau.benoit.free.fr/",
       description="A library to manipulate various data formats and network protocols",
       long_description=open("README.txt", "r").read(),
-      version="0.2.2",
+      version="0.2.3",
       license="GPLv2",
-      packages=["libmich", "libmich.core", "libmich.formats", "libmich.utils"])
+      packages=["libmich", "libmich.core", "libmich.formats", "libmich.utils", "libmich.asn1", "libmich.mobnet"])
