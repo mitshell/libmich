875d0e657b53f0a7c4d15469ef84d4207e5f783f
mitshell <michau.benoit@gmail.com>
2015-02-19 01:07:13 +0100

    add post install step for compiling ASN.1 modules

--- setup.py
+++ setup.py
@@ -1,4 +1,15 @@
 from distutils.core import setup
+from distutils.command.install_data import install_data
+
+class post_install(install_data):
+    def run(self):
+        # Call parent 
+        install_data.run(self)
+        # Compile ASN.1 modules
+        print('post installation: compiling ASN.1 modules')
+        from libmich.asn1.processor import *
+        generate_modules(MODULES)
+
 
 setup(name="libmich",
       author="Benoit Michau",
@@ -6,6 +17,7 @@ setup(name="libmich",
       url="http://michau.benoit.free.fr/",
       description="A library to manipulate various data formats and network protocols",
       long_description=open("README.txt", "r").read(),
-      version="0.2.3",
+      version="0.3.0",
       license="GPLv2",
-      packages=["libmich", "libmich.core", "libmich.formats", "libmich.utils", "libmich.asn1", "libmich.mobnet"])
+      packages=["libmich", "libmich.core", "libmich.formats", "libmich.utils", "libmich.asn1", "libmich.mobnet"],
+      cmdclass={'install_data':post_install})
